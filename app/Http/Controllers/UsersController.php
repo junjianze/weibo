1756ac22ec80292a123f01003990d82f383ff998
junjianze <junjian.ze@seizdata.com>
2021-04-28 07:47:36 +0000

    fang wen ce lv

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -39,21 +39,30 @@ class UsersController extends Controller
     }
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(User $user, Request $request)
     {
+        $this->authorize('update', $user);
         $this->validate($request, [
             'name' => 'required|max:50',
             'password' => 'required|confirmed|min:6'
         ]);
-
         $user->update([
             'name' => $request->name,
             'password' => bcrypt($request->password),
         ]);
-
         return redirect()->route('users.show', $user->id);
+    }
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
     }
 }
